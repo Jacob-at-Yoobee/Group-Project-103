cbf7f19b0c9d60b35716f8abd7478602a6198626
Technoblade <william.giles2005@gmail.com>
2023-06-19 12:25:02 +1200

    login system start

--- Group Project/source/cliTools.cpp
+++ Group Project/source/cliTools.cpp
@@ -119,3 +119,14 @@ double GetInput::getNumber(string _color, string _pre)
 
   return number;
 }
+
+void Clear() // clear the console
+{
+#if defined _WIN32
+  system("cls");
+#elif defined(__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+  system("clear");
+#elif defined(__APPLE__)
+  system("clear");
+#endif
+}

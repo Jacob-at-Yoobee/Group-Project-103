98ce674c388019dad70932c1a3c0a977edc9c7c7
Guntar <131307803+Guntar04@users.noreply.github.com>
2023-06-21 09:35:06 +1200

    Added a Sign Up for Teacher
    I have created a file for the sign ups but have only got the layout and isnt taking any inputs. Parents are to be done next

--- Group Project/source/Menu_register.cpp
+++ Group Project/source/Menu_register.cpp
@@ -5,7 +5,33 @@ using namespace std;
 
 int MenuRegister::execute()
 {
-  cout << C.blue("Register wow") << endl;
+	int userOccupation;
+	cout << C.blue("Occupation") << endl << "1)Teacher\n2)Parent";
+	cin >> userOccupation;
+	switch (userOccupation)
+	{
+	case 1:
+		cout << "Teacher Registration" << endl << endl;
+		cout << endl << "Full Name: ";
+
+		cout << endl << "Gender: ";
+
+		cout << endl << "Date of Birth: ";
+
+		cout << endl << "Email: ";
+
+		cout << endl << "Contact Number: ";
+
+		cout << endl << "Classroom Number: ";
+
+		cout << endl << "Teaching Year(eg: year 1): ";
+
+		cout << endl << "Username: ";
+
+		cout << endl << "Password: ";
+		
+		system("pause");
+	}
 
   return 0;
 }

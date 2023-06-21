ffec82ce90541dd1294f68f3b2db5835eb1f38fd
Guntar <131307803+Guntar04@users.noreply.github.com>
2023-06-22 11:44:20 +1200

    Student Records update

--- Group Project/header/Student_records.h
+++ Group Project/header/Student_records.h
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+#include "cliTools.h"
+
 using namespace std;
 
 class StudentRecord
@@ -9,4 +11,9 @@ class StudentRecord
 
 public:
 	int execute();
+	//void addStudent();
+	//void editStudent();
+	void deleteStudent();
+	//void updateStudent();
+	//void viewStudent();
 };
--- Group Project/source/Student_records.cpp
+++ Group Project/source/Student_records.cpp
@@ -12,26 +12,31 @@ int StudentRecord::execute()
 	cout << "4) Update Record\n";
 	cout << "5) View the Records\n";
 	cout << "Enter a number: ";
-	cin >> opt;
+	opt = getInput.getNumber();
 	if (opt == 1)
 	{
 		//addStudent();
+		//this->addStudent();
 	}
 	else if (opt == 2)
 	{
 		//editRecords();
+		//this->editStudent();
 	}
 	else if (opt == 3)
 	{
 		//deleteRecords();
+		this->deleteStudent();
 	}
 	else if (opt == 4)
 	{
 		//updateRecord();
+		//this->updateStudent();
 	}
 	else if (opt == 5)
 	{
 		//viewRecords();
+		//this->viewStudent();
 	}
 	else
 	{
@@ -40,3 +45,44 @@ int StudentRecord::execute()
 	}
 	return 0;
 }
+
+
+//int StudentRecord::addStudent() {
+
+//}
+
+//int StudentRecord::editStudent() {
+
+//}
+
+void StudentRecord::deleteStudent() {
+	string deleteStudent;
+	cout << "Delete Student Record\n\n";
+	cout << "Enter Students Name and Class:\n\n";
+
+	cout << "Student's Name: ";
+
+	cout << "Student's Class: ";
+
+	//Add if statement to check if that student exists
+
+	//Else if student does exsist
+	cout << "Type 'Delete' to confirm! ";
+	deleteStudent = getInput.getString();
+	deleteStudent[0] = toupper(deleteStudent[0]);
+	if (deleteStudent == "Delete") 
+	{
+		//Removes student from the files/database
+		cout << "Student's Record Removed!";
+	}
+
+}
+
+
+//int StudentRecord::updateStudent() {
+
+//}
+
+//int StudentRecord::viewStudent() {
+
+//}
